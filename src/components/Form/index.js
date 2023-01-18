5d524e6d5f0bb5aca9645af9f53683b50dc0b36d
DevRuiz18 <gustavosruiz@gmail.com>
2023-01-18 18:39:20 -0300

    first commit part 2 done

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,34 @@
 import './Form.css';
 import TextField from '../TextField';
+import DropDownList from '../DropDownList';
+import Button from '../Button';
 
 const Form = () => {
+    const teams = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ];
+
+    const onSaving = (event) => {
+        event.preventDefault();
+    }
+
     return(
-        <section className="form">
+        <section className="form" onSubmit={onSaving}>
             <form>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite seu nome"></TextField>
-                <TextField label="Cargo" placeholder="Digite seu cargo"></TextField>
-                <TextField label="Imagem" placeholder="Digite o endereço da imagem"></TextField>
+                <TextField required={true} label="Nome" placeholder="Digite seu nome"></TextField>
+                <TextField required={true} label="Cargo" placeholder="Digite seu cargo"></TextField>
+                <TextField required={true}label="Imagem" placeholder="Digite o endereço da imagem"></TextField>
+                <DropDownList required={true} label="Time" itens={teams}/>
+                <Button >
+                    Criar Card
+                </Button>
             </form>
         </section>
     );
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,11 +1,10 @@
 import './TextField.css';
 
 const TextField = (props) =>{
-
     return(
         <div className="field-text">
             <label>{props.label}</label>
-            <input placeholder={`${props.placeholder}`}/>  
+            <input required={props.required} placeholder={`${props.placeholder}`}/>  
         </div>
     );
 }
