5d49406bb3dfaf35c943968b379fbdeb180e1e37
DevRuiz18 <gustavosruiz@gmail.com>
2023-01-19 21:16:41 -0300

    final project transeted to pt-br

--- src/components/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-import './Banner.css';
-
-export const  Banner = () => {
-    return(
-        <header className='banner'>
-            <img src="/images/banner.png" alt="Banner principal da pagina do organograma"></img>
-        </header>
-    )
-}
